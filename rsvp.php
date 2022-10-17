a26a3a53e37f8fa6269d1742c5728a1e11b0ba66
Surya <surya.nata.aardhana@gmail.com>
2022-10-17 15:28:38 +0700

    memperbaiki responsive

--- rsvp.php
+++ rsvp.php
@@ -14,7 +14,21 @@
     <div class="container3">
         <div class="container"></div>
     </div>
-
+    <div class="fixed-bottom">
+        <div class="navigation">
+            <ul class="d-flex ps-0 pt-5" style="list-style-type: none; justify-content: space-around">
+                <a href="pengantin.html" style="text-decoration: none">
+                    <li class="navbar-item active">Pengantin</li>
+                </a>
+                <a href="jadwal.html" style="text-decoration: none">
+                    <li class="navbar-item">Jadwal</li>
+                </a>
+                <a href="#" style="text-decoration: none">
+                    <li class="navbar-item">RSVP</li>
+                </a>
+            </ul>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
     </script>
